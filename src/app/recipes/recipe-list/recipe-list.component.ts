e8df9f29264e5110938c88212c3979d0ec939eee
Lester Cuadra <lester.cuadra24@gmail.com>
2023-07-15 20:43:07 +0800

    feat: model + recipe ui

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { Recipe } from 'src/app/models/recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe(
+      'Chicken Adobo',
+      'Chicken Adobo recipe',
+      'https://upload.wikimedia.org/wikipedia/commons/3/38/Chicken_adobo.jpg'
+    ),
+  ];
 }
