877c2e7fa1698383256f80c3a7923e0f06af71a0
Lester Cuadra <lester.cuadra24@gmail.com>
2023-07-21 21:29:57 +0800

    feat: selecting recipe

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { Recipe } from 'src/app/models/recipe.model';
 
 @Component({
@@ -7,11 +7,30 @@ import { Recipe } from 'src/app/models/recipe.model';
   styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
-    new Recipe(
-      'Chicken Adobo',
-      'Chicken Adobo recipe',
-      'https://upload.wikimedia.org/wikipedia/commons/3/38/Chicken_adobo.jpg'
-    ),
+    {
+      name: 'Chicken Adobo',
+      description: 'Chicken Adobo recipe',
+      imagePath:
+        'https://upload.wikimedia.org/wikipedia/commons/3/38/Chicken_adobo.jpg',
+    },
+    {
+      name: 'Bistek Tagalog',
+      description: 'Bistek Tagalog recipe',
+      imagePath:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Bistek_Tagalog-02.jpg/640px-Bistek_Tagalog-02.jpg',
+    },
+    {
+      name: 'Beef Broccoli',
+      description: 'Beef Broccoli recipe',
+      imagePath:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/Broccoli_beef_in_hoisin_sauce_%284667228610%29.jpg/1200px-Broccoli_beef_in_hoisin_sauce_%284667228610%29.jpg',
+    },
   ];
+
+  onRecipeSelected(recipe: Recipe): void {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
