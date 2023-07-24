720b2603dc9637ce57731b5f6f3e36be40ffd10e
Lester Cuadra <lester.cuadra24@gmail.com>
2023-07-24 14:02:13 +0800

    misc: added recipe ingredient

--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -11,18 +11,30 @@ export class RecipeService {
       description: 'Chicken Adobo recipe',
       imagePath:
         'https://upload.wikimedia.org/wikipedia/commons/3/38/Chicken_adobo.jpg',
+      ingredients: [
+        { name: 'Salt', amount: 1 },
+        { name: 'Soy Sauce', amount: 1 },
+      ],
     },
     {
       name: 'Bistek Tagalog',
       description: 'Bistek Tagalog recipe',
       imagePath:
         'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Bistek_Tagalog-02.jpg/640px-Bistek_Tagalog-02.jpg',
+      ingredients: [
+        { name: 'Pepper', amount: 2 },
+        { name: 'Onion', amount: 2 },
+      ],
     },
     {
       name: 'Beef Broccoli',
       description: 'Beef Broccoli recipe',
       imagePath:
         'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/Broccoli_beef_in_hoisin_sauce_%284667228610%29.jpg/1200px-Broccoli_beef_in_hoisin_sauce_%284667228610%29.jpg',
+      ingredients: [
+        { name: 'Beef', amount: 3 },
+        { name: 'Broccoli', amount: 3 },
+      ],
     },
   ];
 
