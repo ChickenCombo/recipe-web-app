90bced4da4cf76e23a07e0098e17a4452758ead7
Lester Cuadra <lester.cuadra24@gmail.com>
2023-08-29 11:57:30 +0800

    feat: firebase signup

--- src/app/components/auth/auth.component.ts
+++ src/app/components/auth/auth.component.ts
@@ -1,10 +1,47 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.css']
+  styleUrls: ['./auth.component.css'],
 })
 export class AuthComponent {
+  isLoginMode = true;
+  isLoading = false;
+  error: string | null = null;
 
+  constructor(private authService: AuthService) {}
+
+  onSwitchMode(): void {
+    this.isLoginMode = !this.isLoginMode;
+  }
+
+  onSubmit(form: NgForm): void {
+    if (!form.valid) {
+      return;
+    }
+
+    this.isLoading = true;
+
+    if (this.isLoginMode) {
+    } else {
+      const email = form.value.email;
+      const password = form.value.password;
+
+      this.authService.signUp(email, password).subscribe(
+        (res) => {
+          console.log(res);
+          this.isLoading = false;
+        },
+        (error) => {
+          this.error = error;
+          this.isLoading = false;
+        }
+      );
+    }
+
+    form.reset();
+  }
 }
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,50 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, catchError, throwError } from 'rxjs';
+
+interface AuthResponseData {
+  kind: string;
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+}
+
+@Injectable({ providedIn: 'root' })
+export class AuthService {
+  constructor(private http: HttpClient) {}
+
+  signUp(email: string, password: string): Observable<AuthResponseData> {
+    return this.http
+      .post<AuthResponseData>(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCqeYzndy0RQcfIYy0NPeq2Y1ElJWliqWY',
+        { email, password, returnSecureToken: true }
+      )
+      .pipe(
+        catchError((error) => {
+          let errorMessage = 'An unknown error occured!';
+
+          if (!error.error || !error.error.error) {
+            return throwError(() => errorMessage);
+          }
+
+          switch (error.error.error.message) {
+            case 'EMAIL_EXISTS':
+              errorMessage =
+                'The email address is already in use by another account.';
+              break;
+            case 'TOO_MANY_ATTEMPTS_TRY_LATER':
+              errorMessage =
+                'We have blocked all requests from this device due to unusual activity. Try again later.';
+              break;
+            case 'OPERATION_NOT_ALLOWED':
+              errorMessage = 'Password sign-in is disabled for this project.';
+              break;
+          }
+
+          return throwError(() => errorMessage);
+        })
+      );
+  }
+}
